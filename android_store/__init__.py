7fec0063c8be3e2e5cf90da180447d2db53ccf19
Bardia Moshiri <bardia@furilabs.com>
2025-05-18 17:11:27 -0400

    treewide: add missing license headers
    Signed-off-by: Bardia Moshiri <bardia@furilabs.com>

--- android_store/__init__.py
+++ android_store/__init__.py
@@ -1,3 +1,6 @@
+# SPDX-License-Identifier: GPL-2.0-only
+# Copyright (C) 2025 Bardia Moshiri <bardia@furilabs.com>
+
 from .android_store import AndroidStoreService
 
 __all__ = ['AndroidStoreService']
--- open_store/__init__.py
+++ open_store/__init__.py
@@ -1,3 +1,6 @@
+# SPDX-License-Identifier: GPL-2.0-only
+# Copyright (C) 2025 Bardia Moshiri <bardia@furilabs.com>
+
 from .open_store import OpenStoreService
 
 __all__ = ['OpenStoreService']
